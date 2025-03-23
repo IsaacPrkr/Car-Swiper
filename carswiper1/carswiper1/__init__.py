e8185a658ce21d3ea976c16966bc2be1fa432c76
Isaac Parker <98621223+IsaacPrkr@users.noreply.github.com>
2025-03-23 14:21:53 +0000

    .

--- carswiper1/carswiper1/__init__.py
+++ carswiper1/carswiper1/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List, Annotated
-from . import models
-from .database import engine, SessionLocal
+import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-
+from . import models
+from .database import engine, SessionLocal
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
