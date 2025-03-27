77032fd834eab91e6d9c10c599d17c564a93d7bf
Isaac Parker <98621223+IsaacPrkr@users.noreply.github.com>
2025-03-27 01:13:16 +0000

    .

--- carswiper/src/carswiper/main.py
+++ carswiper/src/carswiper/main.py
@@ -4,8 +4,8 @@ from typing import List, Annotated
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from . import models
-from .database import engine, SessionLocal
+
+
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
@@ -26,11 +26,3 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
-
-
-
-def main() -> None:
-    print("Hello from carswiper1!")
-
-
-#uv run carswiper1
