37624505cbb57300bf10a9f5f1c11f1eecaf6cde
Isaac Parker <98621223+IsaacPrkr@users.noreply.github.com>
2025-03-31 17:37:28 +0100

    implementing first api endpoints (registration)

--- src/carswiper/main.py
+++ src/carswiper/main.py
@@ -1,15 +1,21 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr
 from sqlalchemy.orm import Session
 
-from carswiper import models
+from carswiper import models, schemas
 from carswiper.database import SessionLocal, engine
+
+from passlib.context import CryptContext
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def password_hash(password):
+    return pwd_context.hash(password)
 
 # Defining Models for APIs
 class UserCreate(BaseModel):
@@ -31,5 +37,17 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/register")
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_password = password_hash(user.password)
+    db_user = models.User(username=user.username, email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return {"username": db_user.username, "email": db_user.email}
 
 #Uv run python src/carswiper/main.py
