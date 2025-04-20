c86b0640c1113d23d9c16e0583db5568a490a507
Isaac Parker <98621223+IsaacPrkr@users.noreply.github.com>
2025-04-20 13:19:30 +0100

    designing pages in tailwind

--- src/carswiper/car-swiper-frontend/app/register/page.js
+++ src/carswiper/car-swiper-frontend/app/register/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import axios from "axios";
+import "./register.css";
 
 export default function Register() {
   const [formData, setFormData] = useState({ username: "", email: "", password: "" });
@@ -31,25 +32,25 @@ export default function Register() {
             alt="logo"
           />
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create an account
+        <div className="register-container">
+          <div className="register-content">
+            <h1 className="register-heading">
+              Create an Account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="username"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className=".register-label"
                 >
-                  Your username
+                  Username
                 </label>
                 <input
                   type="text"
                   name="username"
                   id="username"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Your username"
+                  placeholder="Username"
                   required
                   onChange={handleChange}
                 />
@@ -59,7 +60,7 @@ export default function Register() {
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your email
+                  Email
                 </label>
                 <input
                   type="email"
