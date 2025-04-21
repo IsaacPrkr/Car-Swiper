2aa614b4b09c4a5b585dc9e50667e531ecb22810
Isaac Parker <98621223+IsaacPrkr@users.noreply.github.com>
2025-04-21 23:33:34 +0100

    styled login page

--- src/carswiper/car-swiper-frontend/app/login/page.js
+++ src/carswiper/car-swiper-frontend/app/login/page.js
@@ -27,21 +27,21 @@ export default function Login() {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img
-            className="w-50 h-50 mr-2"
+            className="login-logo"
             src="/logosmall.png" 
             alt="logo"
           />
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div className="login-container">
+          <div className="login-content">
+            <h1 className="login-heading">
               Log In 
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor="username"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="login-label"
                 >
                   Username:
                 </label>
@@ -49,8 +49,8 @@ export default function Login() {
                   type="text"
                   name="username"
                   id="username"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="username"
+                  className="login-input"
+                  placeholder="Username"
                   required
                   onChange={handleChange}
                 />
@@ -58,7 +58,7 @@ export default function Login() {
               <div>
                 <label
                   htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="login-label"
                 >
                   Password:
                 </label>
@@ -67,14 +67,14 @@ export default function Login() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="login-input"
                   required
                   onChange={handleChange}
                 />
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="login-button"
               >
                 Log in
               </button>
