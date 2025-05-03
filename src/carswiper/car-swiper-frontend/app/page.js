36139de338cbe6865ec51f4955bce0fc224c1626
Isaac Parker <98621223+IsaacPrkr@users.noreply.github.com>
2025-05-03 22:48:21 +0100

    .

--- src/carswiper/car-swiper-frontend/app/page.js
+++ src/carswiper/car-swiper-frontend/app/page.js
@@ -1,8 +1,8 @@
-import "./initialpage.css";
+import "./home.css";
 
 export default function Home() {
     return (
-      <section className="home-page">
+      <section className="initial-page">
         <h1 className="text-3xl font-bold text-center mt-10">Welcome to Car Swiper</h1>
         <p className="text-center mt-4">Find your dream car today!</p>
       </section>
