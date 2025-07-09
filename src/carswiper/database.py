32d3ab99fd52a5c034a41120482cb73b10aa11db
Isaac Parker <98621223+IsaacPrkr@users.noreply.github.com>
2025-07-09 19:17:46 +0100

    Update database.py

--- src/carswiper/database.py
+++ src/carswiper/database.py
@@ -2,11 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL environment variable not set")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:NaoYu123@localhost:5432/CarSwiper")
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
